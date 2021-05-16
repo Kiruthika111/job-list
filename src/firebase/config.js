7f8fe47d34ce43dd8309423fcc8bdf547134d4dd
Kiruthika Krishnamoorthy <62352607+Kiruthika111@users.noreply.github.com>
2021-05-16 08:15:07 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,14 +2,14 @@ import app from 'firebase/app';
 import 'firebase/firestore';
 
 var firebaseConfig = {
-  apiKey: "AIzaSyDqy7g0zRRQuJJN-iK45Qw5fbdLOJRVY1o",
-  authDomain: "portal-9adb4.firebaseapp.com",
-  databaseURL: "https://portal-9adb4.firebaseio.com",
-  projectId: "portal-9adb4",
-  storageBucket: "portal-9adb4.appspot.com",
-  messagingSenderId: "520915628300",
-  appId: "1:520915628300:web:82b13a0ab7653086b3d929",
-  measurementId: "G-3CPHQXZVKR"
+  apiKey: "xxxxxxxxxxxxxxxxxx-xxxxxxxxxxxxxxxx1o",
+  authDomain: "portal-xxxxx.firebaseapp.com",
+  databaseURL: "https://portal-xxxxx.firebaseio.com",
+  projectId: "portal-xxxxx",
+  storageBucket: "portal-xxxxx.appspot.com",
+  messagingSenderId: "xxxxxxxxxxxx",
+  appId: "x:xxxxxxxxxxx:web:82bxxxxxxxxxxxxb3d929",
+  measurementId: "x-xxxxxxxxxx"
 };
 
 const firebase = app.initializeApp(firebaseConfig);
